40ae53ec0bb6c259f56eebef892c78d8a72bf05e
Merak Bourque <xtremeyou@hotmail.com>
2024-07-23 12:54:53 -0400

    refactored breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,17 @@
 const needle = require("needle");
-const args = process.argv.slice(2);
-const url = args[0];
+const args = process.argv
+const url = args[2];
 const urlLink = "https://api.thecatapi.com/v1/breeds/search?q=";
-const specificPet = url;
 
-//need to somehow change the end of the url to a variable that gets tied to my arg url
-needle.get(urlLink + specificPet, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    let firstEntry = body.map(({ description }) => description);
-    console.log(firstEntry);
-  } else {
-    console.log("error:", error); // Print the error if one occurred
-  }
-});
+const fetchBreedDescription = function (breedName, callback) {
+  needle.get(urlLink + url, (error, response, body) => {
+    if (!error && response) {
+      let firstEntry = body.map(({ description }) => description);
+      callback(null, firstEntry);
+    } else {
+        callback(error, null)
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
